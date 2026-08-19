6ce5341b0b714304885be545e652a887ed9faf3a
Kevin <kevin.quispe.cc@tecsup.edu.pe>
2026-08-19 09:12:47 -0500

    Initial Commit

---
+++ Laboratorio_00/Laboratorio_00App.swift
@@ -0,0 +1,17 @@
+//
+//  Laboratorio_00App.swift
+//  Laboratorio_00
+//
+//  Created by Tecsup on 19/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Laboratorio_00App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
