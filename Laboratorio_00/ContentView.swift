a8e8ed4a733edd8003a84623219377999c326802
Kevin <kevin.quispe.cc@tecsup.edu.pe>
2026-08-19 09:36:22 -0500

    IOs mejorado con icono

--- Laboratorio_00/ContentView.swift
+++ Laboratorio_00/ContentView.swift
@@ -2,23 +2,29 @@
 //  ContentView.swift
 //  Laboratorio_00
 //
-//  Created by Tecsup on 19/08/26.
+//  Created by Tecsup on 19/08/26.   
 //
 
 import SwiftUI
-
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
+        VStack(spacing: 20) {
+            Image(systemName: "swift")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundStyle(.orange)
+                .font(.system(size: 60))
+
+            Text("¡Hola, iOS Avanzado!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+
+            Text("Mi primer commit desde Xcode")
+                .font(.subheadline)
+                .foregroundColor(.gray)
         }
         .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
